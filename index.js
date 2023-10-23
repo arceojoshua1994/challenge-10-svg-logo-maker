adeeccf84dff4e199f616981409002a7f75da484
arceojoshua94 <arceojoshua94@outlook.com>
2023-10-23 15:00:53 -0400

    create repository and add index

---
+++ index.js
@@ -0,0 +1,6 @@
+const Shape = require('./Shape');
+const Circle = require('./Circle');
+const Square = require('./Square');
+const Triangle = require('./Triangle');
+
+module.exports = { Shape, Circle, Square, Triangle };
